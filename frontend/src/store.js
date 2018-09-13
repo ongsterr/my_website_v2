1780600d3f2ce5aa97cf6e69c656e62a555d49ff
ongsterr <ong.chris11@gmail.com>
2018-09-13 23:18:50 +1000

    chore: add reducers to redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,9 +4,13 @@ import { promiseMiddleware, localStorageMiddleware } from './middleware';
 import auth from 'reducers/auth';
 import common from 'reducers/common';
 import article from 'reducers/article';
+import articleList from 'reducers/articleList';
+import editor from 'reducers/editor';
 
 const reducer = combineReducers({
+  articleList,
   article,
+  editor,
   common,
   auth,
 });
