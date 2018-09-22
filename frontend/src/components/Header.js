c84957ccda3fe786f8cb22a4a9f04602144f1f75
ongsterr <ong.chris11@gmail.com>
2018-09-22 21:33:49 +1000

    chore: update navbar item

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-import ChrisIcon from 'assets/icons/ChrisIcon';
+import ChrisIcon from 'assets/icons/ChrisIcon'
 
 const LoggedOutView = ({ currentUser }) =>
-  !currentUser ? (
-    <div className="right menu">
-      <Link to="/articles" className="item">
-        Thoughts
-      </Link>
-      <Link to="/about" className="item">
-        About
-      </Link>
-      <Link to="/login" className="item">
-        Sign in
-      </Link>
-    </div>
-  ) : null;
+	!currentUser ? (
+		<div className="right menu">
+			<Link to="/articles" className="item">
+				Thoughts
+			</Link>
+			<Link to="/about" className="item">
+				About
+			</Link>
+			<Link to="/login" className="item">
+				Me Time
+			</Link>
+		</div>
+	) : null
 
 const LoggedInView = ({ currentUser }) =>
-  currentUser ? (
-    <div className="right menu">
-      <Link to="/articles" className="item hover">
-        Thoughts
-      </Link>
-      <Link to="/about" className="item">
-        About
-      </Link>
-      <Link to="/editor" className="item">
-        New Post
-      </Link>
-    </div>
-  ) : null;
+	currentUser ? (
+		<div className="right menu">
+			<Link to="/articles" className="item hover">
+				Thoughts
+			</Link>
+			<Link to="/about" className="item">
+				About
+			</Link>
+			<Link to="/editor" className="item">
+				New Post
+			</Link>
+		</div>
+	) : null
 
-const Header = ({ appName, currentUser }) => (
-  <nav className="ui large secondary menu" style={{ marginBottom: 0 }}>
-    <Link to="/" className="">
-      <ChrisIcon />
-    </Link>
+const Header = ({ currentUser }) => (
+	<nav className="ui large secondary menu" style={{ marginBottom: 0 }}>
+		<Link to="/" className="">
+			<ChrisIcon />
+		</Link>
 
-    <LoggedOutView currentUser={currentUser} />
-    <LoggedInView currentUser={currentUser} />
-  </nav>
-);
+		<LoggedOutView currentUser={currentUser} />
+		<LoggedInView currentUser={currentUser} />
+	</nav>
+)
 
-export default Header;
+export default Header
