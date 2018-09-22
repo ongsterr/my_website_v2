1b957046a2aca15d4d79c1216168742f7ad37e29
ongsterr <ong.chris11@gmail.com>
2018-09-22 13:24:20 +1000

    feat: update profile pic with random avatar

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -1,112 +1,120 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import marked from 'marked';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import marked from 'marked'
 
-import api from 'api';
+import api from 'api'
 
 const mapStateToProps = state => ({
-  ...state.articleList,
-  currentUser: state.common.currentUser,
-  profile: state.profile,
-});
+	...state.articleList,
+	currentUser: state.common.currentUser,
+	profile: state.profile,
+})
 
 const mapDispatchToProps = dispatch => ({
-  onLoad: payload => dispatch({ type: 'PROFILE_PAGE_LOADED', payload }),
-  onUnload: () => dispatch({ type: 'PROFILE_PAGE_UNLOADED' }),
-  onSetPage: (page, payload) => dispatch({ type: 'SET_PAGE', page, payload }),
-});
+	onLoad: payload => dispatch({ type: 'PROFILE_PAGE_LOADED', payload }),
+	onUnload: () => dispatch({ type: 'PROFILE_PAGE_UNLOADED' }),
+	onSetPage: (page, payload) => dispatch({ type: 'SET_PAGE', page, payload }),
+})
 
 const workProgression = () => (
-  <div className="sub header">
-    <div className="ui large breadcrumb">
-      <div className="section">Process Engineer</div>
-      <i className="right chevron icon divider" />
-      <div className="section">Finance Manager</div>
-      <i className="right chevron icon divider" />
-      <div className="active section">Web Developer</div>
-    </div>
-  </div>
-);
+	<div className="sub header">
+		<div className="ui large breadcrumb">
+			<div className="section">Process Engineer</div>
+			<i className="right chevron icon divider" />
+			<div className="section">Finance Manager</div>
+			<i className="right chevron icon divider" />
+			<div className="active section">Web Developer</div>
+		</div>
+	</div>
+)
+
+const randomRobot = [
+	'chrisongg',
+	'christabel',
+	'leemun',
+	'hello-world',
+	'ongsterr',
+][Math.round(Math.random() * 5)]
 
 const quoteBlocks = (
-  <div
-    style={{
-      backgroundImage: `url(
-              https://s3-ap-southeast-2.amazonaws.com/personalwebsiteforchris/Pictures/chris.png
+	<div
+		style={{
+			backgroundImage: `url(
+        https://robohash.org/${randomRobot}.png
             )`,
-    }}
-    className="link dt hide-child br-100 w5 h-100 cover bg-center">
-    <div className="pv4-5 ph4-5 br-100 tc w-100 h-100 child bg-black-20">
-      <h3 className="ui header">
-        <div className="white">If you're going to</div>
-        <div className="white">have a story,</div>
-        <div className="white">have a big story,</div>
-        <div className="white">or none at all.</div>
-      </h3>
-      <div className="sub header white">- J. Campbell</div>
-    </div>
-  </div>
-);
+		}}
+		className="link dt hide-child br-100 w5 h-100 cover bg-center">
+		<div className="pv4-5 ph4-5 br-100 tc w-100 h-100 child bg-black-20">
+			<h3 className="ui header">
+				<div className="white">If you're going to</div>
+				<div className="white">have a story,</div>
+				<div className="white">have a big story,</div>
+				<div className="white">or none at all.</div>
+			</h3>
+			<div className="sub header white">- J. Campbell</div>
+		</div>
+	</div>
+)
 
 const EditProfileSettings = ({ isUser }) =>
-  isUser ? (
-    <button className="ui primary basic small button">
-      <Link to="/about/edit">
-        <i className="edit icon" />
-        Edit Profile Settings
-      </Link>
-    </button>
-  ) : null;
+	isUser ? (
+		<button className="ui primary basic small button">
+			<Link to="/about/edit">
+				<i className="edit icon" />
+				Edit Profile Settings
+			</Link>
+		</button>
+	) : null
 
 class About extends Component {
-  componentWillMount() {
-    this.props.onLoad(Promise.all([api.Profile.get('chrisongg')]));
-  }
+	componentWillMount() {
+		this.props.onLoad(Promise.all([api.Profile.get('chrisongg')]))
+	}
 
-  componentWillUnmount() {
-    this.props.onUnload();
-  }
+	componentWillUnmount() {
+		this.props.onUnload()
+	}
 
-  render() {
-    const { profile, currentUser } = this.props;
-    const isUser = currentUser && profile.username === currentUser.username;
-    const markup = profile.bio ? { __html: marked(profile.bio) } : null;
-    const noBio = markup ? null : <div>My story is coming...</div>;
+	render() {
+		const { profile, currentUser } = this.props
+		const isUser = currentUser && profile.username === currentUser.username
+		const markup = profile.bio ? { __html: marked(profile.bio) } : null
+		const noBio = markup ? null : <div>My story is coming...</div>
 
-    return (
-      <div>
-        <div className="mb3">
-          <h1 className="ui header">
-            <div className="mb3">
-              <span>I'm Chris.</span>
-              <br />
-              <span>Your problem solver.</span>
-            </div>
-          </h1>
-          <div className="ui grid">
-            <div className="ui two column row">
-              <div className="left floated column">{workProgression()}</div>
-              <div className="right floated right aligned column">
-                <EditProfileSettings isUser={isUser} />
-              </div>
-            </div>
-          </div>
-        </div>
+		return (
+			<div>
+				<div className="mb3">
+					<h1 className="ui header">
+						<div className="mb3">
+							<span>I'm Chris.</span>
+							<br />
+							<span>Your problem solver.</span>
+						</div>
+					</h1>
+					<div className="ui grid">
+						<div className="ui two column row">
+							<div className="left floated column">{workProgression()}</div>
+							<div className="right floated right aligned column">
+								<EditProfileSettings isUser={isUser} />
+							</div>
+						</div>
+					</div>
+				</div>
 
-        <div className="flex flex-column">
-          <div className="mr4 mb4 self-center">{quoteBlocks}</div>
-          <div>
-            <div dangerouslySetInnerHTML={markup} />
-            {noBio}
-          </div>
-        </div>
-      </div>
-    );
-  }
+				<div className="flex flex-column">
+					<div className="mr4 mb4 self-center">{quoteBlocks}</div>
+					<div>
+						<div dangerouslySetInnerHTML={markup} />
+						{noBio}
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(About);
+	mapStateToProps,
+	mapDispatchToProps
+)(About)
